95c895f1e6cdb461b699da9907e4864330d8d438
IMarcusDev <escobarmarcos511@gmail.com>
2025-02-01 12:54:56 -0500

    backend config hotfix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/api', // Cambia esto según tu backend
+  baseURL: 'http://localhost:5000/api', // Cambia esto según tu backend
   timeout: 5000,
 });
 
